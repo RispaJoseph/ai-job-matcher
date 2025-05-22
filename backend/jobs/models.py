fd799ef6b153cf9061b9166607e2af74a2c9bb0c
RispaJoseph <rizpahjoseph@gmail.com>
2025-05-22 21:43:01 +0530

    added jobs models

--- backend/jobs/models.py
+++ backend/jobs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+
+class Job(models.Model):
+    recruiter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
